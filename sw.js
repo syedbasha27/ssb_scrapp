c109465906b9e6d36b7777ecba3665054f7f51fa
S SYED BASHA <syedbashashaik517@gmail.com>
2026-02-25 15:50:20 +0530

    Update service worker cache version to v1.0.0.14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ssb-scrap-v1.0.0.13';
+const CACHE_NAME = 'ssb-scrap-v1.0.0.14';
 const APP_SHELL = [
   './',
   './index.html',
@@ -60,6 +60,7 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
 
 
